05ef2e1d9e992ea4a9c068dc395c3c9e20218c6a
lcc <sub01@iaonline.net>
2020-10-15 22:45:45 -0500

    Initial commit

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -1,14 +1,58 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarNS;
 
 namespace CarTests
 {
+    [TestClass]
     public class CarTests
     {
         //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitHub account)
+        [TestMethod]
+        public void EmptyTest()
+        {
+            Assert.AreEqual(10, 10, .001);
+        }
+
         //TODO: constructor sets gasTankLevel properly
+        Car test_car;
+        
+        [TestInitialize]
+        public void CreateCarObject()
+        {
+            test_car = new Car("Toyota", "Prius", 10, 50);
+        }
+
+        [TestMethod]
+        public void TestInitialGasTank()
+        {
+            //Car test_car = new Car("Toyota", "Prius", 10, 50);
+            Assert.AreEqual(10, test_car.GasTankLevel, 0.001);
+        }
+        
         //TODO: gasTankLevel is accurate after driving within tank range
+        [TestMethod]
+        public void TestGasTankAfterDriving()
+        {
+            test_car.Drive(50);
+            Assert.AreEqual(9, test_car.GasTankLevel, 0.001);
+        }
+
         //TODO: gasTankLevel is accurate after attempting to drive past tank range
+        [TestMethod]
+        public void TestGasTankAfterExceedingTankRange()
+        {
+            test_car.Drive(test_car.GasTankLevel * test_car.MilesPerGallon + 1);
+            Assert.AreEqual(0, test_car.GasTankLevel, 0.001);
+        }
+
         //TODO: can't have more gas than tank size, expect an exception
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void TestGasOverfillException()
+        {
+            test_car.AddGas(5);
+            Assert.Fail("Shouldn't get here, car cannot have more gas in the tank than the size of the tank.");
+        }
 
     }
 }
